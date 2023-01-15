86eccb1d08383591e74798858531663f4ab72cc5
predrag <predrag.mitrovic@sunzinet.com>
2023-01-15 18:56:36 +0100

    basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{name: 'Welcome'}">Welcome</router-link> |
+    <router-link :to="{name: 'Chatroom'}">Chatroom</router-link>
   </div>
   <router-view/>
 </template>
